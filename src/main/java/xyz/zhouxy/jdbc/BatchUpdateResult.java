a6073e34b5625c947ee91ae138bc583943bf96fb
ZhouXY108 <luquanlion@outlook.com>
2026-05-23 04:55:37 +0800

    fix: 修改getUpdateCounts方法返回数组副本避免外部修改内部数据

--- src/main/java/xyz/zhouxy/jdbc/BatchUpdateResult.java
+++ src/main/java/xyz/zhouxy/jdbc/BatchUpdateResult.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2026 the original author or authors.
+ * Copyright 2026-present the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -74,7 +74,8 @@ public class BatchUpdateResult {
      * 获取批次更新结果
      */
     public int[] getUpdateCounts(int batchIndex) {
-        return this.allUpdateCounts.get(batchIndex);
+        int[] updateCounts = this.allUpdateCounts.get(batchIndex);
+        return updateCounts != null ? updateCounts.clone() : null;
     }
 
     /**
